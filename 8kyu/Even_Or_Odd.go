48ae4e7a2b7afbe77ae005a59f5756b6e92f174d
worstp <mikaelmuniz2001@gmail.com>
2022-08-08 16:21:36 -0300

    Exercise 1 - Even Or Odd numbers

---
+++ 8kyu/Even_Or_Odd.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(EvenOrOdd(2))
+	fmt.Println(EvenOrOdd(3))
+}
+
+func EvenOrOdd(number int) string {
+	result := number % 2
+
+	if result == 0 {
+		return "Even"
+	}
+
+	return "Odd"
+}
