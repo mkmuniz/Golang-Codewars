811ff665af58495e216b5feeaebfcedfa919de01
worstp <mikaelmuniz2001@gmail.com>
2022-08-09 21:34:40 -0300

    updating

---
+++ 7kyu/FindOddInt.go
@@ -0,0 +1,24 @@
+package main
+
+func FindOdd(seq []int) int {
+	isCompleted := false
+	count := 0
+	i := 0
+	res := 0
+
+	for !isCompleted {
+		for _, a := range seq {
+			if a == seq[i] {
+				count++
+			}
+		}
+		if count%2 == 1 {
+			res = seq[i]
+			isCompleted = true
+		}
+
+		i++
+	}
+
+	return res
+}
